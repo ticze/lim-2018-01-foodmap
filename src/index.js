5d1e3100d2a05a5395bb20cb3817a661add76f44
ticze <ticze2598@gmail.com>
2018-08-02 13:19:22 -0500

    api google

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
 const preload = document.getElementById('preload');
-setTimeout(()=>{
+setTimeout(() => {
   preload.classList.add('close');
-}, 2000)
+}, 1000)
+
+// maps
+var map, infoWindow;
+function initMap () {
+  map = new google.maps.Map (document.getElementById ('map'), {
+    centro: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Prueba la geolocalización de HTML5.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition (function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition (pos);
+      infoWindow.setContent ('Ubicación encontrada');
+      infoWindow.open (map);
+      map.setCenter (pos);
+    }, function () {
+      handleLocationError (true, infoWindow, map.getCenter ());
+    });
+  } else {
+    // El navegador no admite la geolocalización
+    handleLocationError (false, infoWindow, map.getCenter ());
+  }
+}
+
+function handleLocationError (browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition (pos);
+  infoWindow.setContent (browserHasGeolocation?
+                        'Error: el servicio de geolocalización falló.' :
+                        'Error: su navegador no admite la geolocalización.');
+  infoWindow.open (map);
+}
