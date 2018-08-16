1d02e214698db9aa727b4843005cec513a6cc4da
ticze <ticze2598@gmail.com>
2018-08-16 11:44:10 -0500

    mostrando restaurants

--- src/funcions.js
+++ src/funcions.js
@@ -1,31 +1,17 @@
-window.funtFood = (valFood) => {
-  let localFood = nameFood(valFood.data)
-  if (valFood.search.trim !== '') {
-    localFood = quest(localFood, valFood.search)
-  }
-  return localFood;
-}
-
-window.nameFood = (aliment) =>{
-  console.log(aliment)
-  const arrFood = aliment.map((elemt)=>{
-    const loFood = elemt.food;
-    const forFood = loFood.forEach(element => {
-      return element;
-    });
-    return forFood;
-  })
-  console.log(arrFood)
-  return arrFood;
-}
-
 window.quest = (aliment,inputFood) =>{
   if (inputFood) {
     if (aliment) {
       inputFood = inputFood.toLowerCase();
-      return aliment.filter(aliments => aliments && aliments.food && aliments.food.toLowerCase().indexOf(inputFood) >= -1);
+      return aliment.filter(aliments => aliments.food.toLowerCase().indexOf(inputFood) > -1);
     }
     return [];
   }
   return aliment || [];
+}
+
+window.funtFood = (valFood) => {
+  if (valFood.search.trim !== '') {
+   let localFood = quest(valFood.data, valFood.search)
+  }
+  return localFood;
 }
